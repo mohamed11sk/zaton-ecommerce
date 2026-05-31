6f9928365a5833e522de4fbbdd3a1514ee30e0a6
Mohamed ELsayed <147457783+mohamed11sk@users.noreply.github.com>
2026-06-01 02:24:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connected Successfully";
+    
 } catch (PDOException $e) {
     echo "Connection Failed: " . $e->getMessage();
 }
